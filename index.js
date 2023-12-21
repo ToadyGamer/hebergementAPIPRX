83e8e35871de66035f7c44d9354f58cc2e68ee49
ToadyGamer <castellierflorian@gmail.com>
2023-12-21 17:06:59 +0100

    OK LA CA MARCHE PEUT ETRE

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-	host:'https://databases.000webhost.com/index.php',
-	user:'id21691480_phonerelax',
-	password:'12ff663S!',
-	database:'id21691480_prx'
-});
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  });
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://prorelax.000webhostapp.com');
